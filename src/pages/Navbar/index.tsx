b9396cc89f0cded73f0417e33e6cfec226c4aa08
Arda Mazreku <a.mazreku@aecsolutions.com>
2023-02-13 14:32:05 +0100

    Fixes logo bug & changes favico

--- src/pages/Navbar/index.tsx
+++ src/pages/Navbar/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Navbar = () => (
-    <nav className="bg-primary p-2 flex justify-between items-center space-x-8 uppercase shadow-xl title-primary">
-        <div className="flex items-center">
-            <Link to="/home" className="px-3 py-2 text-white mr-4">
-                <img alt="AEC-logo" src="../../src/assets/images/AEC_Logo.png" className="logo"/>
+    <nav className="bg-primary p-2 flex justify-between space-x-8 uppercase shadow-xl title-primary">
+        <div className="flex float-left items-center">
+            <Link to="/home" className="px-3 py-2 text-white">
+                <img alt="AEC-logo" src="AEC_Logo.png" className="logo brightness-95 inline"/>
             </Link>
             <Link to="/home"
                   className="rounded-lg px-3 py-2 text-white hover:text-gray-800 font-medium mr-4 transition duration-500 ease-in-out">
@@ -16,7 +16,7 @@ const Navbar = () => (
                 About
             </Link>
         </div>
-        <div className="flex float-right">
+        <div className="flex float-right items-center">
             <Link to="/"
                   className="rounded-lg px-3 py-2 text-white hover:text-gray-800 font-medium mr-4 transition duration-500 ease-in-out">
                 Login
