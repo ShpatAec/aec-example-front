366d9eadd677324e00556cae170d2065c82532ba
Nafie Sallahu <123952707+nafiesallahu1@users.noreply.github.com>
2023-02-13 21:22:25 +0100

    Improve cards in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,15 +2,16 @@ import {useGetPosts} from '../../lib/api-hooks';
 import {FetchState} from '../../types';
 import '../../App.css';
 import React from 'react';
-
+import HomeStyle from './home.module.css';
 
 function Home() {
     const [posts, fetchState, getPosts] = useGetPosts();
     const btnOnClick = () => getPosts();
 
     return (
-        <div className="container">
-            <h1 className="text-2xl font-bold text-indigo-400">Rest Api Project Test</h1>
+        <div className={HomeStyle.container}>
+            <h1 className="text-2xl font-bold dark:text-gray-500 uppercase mb-2">Rest Api Project
+                Test</h1>
 
             {fetchState === FetchState.DEFAULT && (
                 <>
@@ -39,18 +40,28 @@ function Home() {
             )}
             {fetchState === FetchState.SUCCESS && (
                 <>
-                    <p className="mb-4 mt-4 text-2xl font-bold tracking-tight text-zinc-600 dark:text-white">Posts:</p>
-                    <ul className="grid grid-cols-4 gap-2">
+                    <div className={HomeStyle.cards}>
+
                         {posts.map((post) => (
-                            <li key={post.id}
-                                className="col-span-2 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-4">
-                                <h3 className="mb-2 text-2xl font-bold tracking-tight text-blue-600 dark:text-white">
-                                    {post.id} - {post.title}
-                                </h3>
-                                <p className="indent-6 italic text-violet-400">{post.body}</p>
-                            </li>
+                            <div key={post.id}
+                                 className="w-[280px] p-5 mb-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                <a href="#">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">  {post.title}</h5>
+                                </a>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.body}</p>
+                                <a href="#"
+                                   className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Read more
+                                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor"
+                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"></path>
+                                    </svg>
+                                </a>
+                            </div>
                         ))}
-                    </ul>
+                    </div>
                 </>
             )}
         </div>
