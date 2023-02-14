356d6a9bc30b579a93a53f5821d93aaeda8b6410
ShpatAec <123937506+ShpatAec@users.noreply.github.com>
2023-02-14 13:15:14 +0100

    Assets Structure with images and fonts small changes in UI Navbar and Cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -46,7 +46,7 @@ function Home() {
                             <div key={post.id}
                                  className="w-[280px] p-5 mb-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                                 <a href="#">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">  {post.title}</h5>
+                                    <h5 className="mb-2 truncate text-2xl font-bold tracking-tight text-gray-900 dark:text-white">  {post.title}</h5>
                                 </a>
                                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.body}</p>
                                 <a href="#"
--- src/pages/Navbar/index.tsx
+++ src/pages/Navbar/index.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Navbar = () => (
-    <nav className="bg-primary p-2 flex justify-between space-x-8 uppercase shadow-xl title-primary">
+    <nav className="bg-white sticky top-0 p-2 flex justify-between space-x-8 uppercase shadow-xl title-primary">
         <div className="flex float-left items-center">
             <Link to="/home" className="px-3 py-2 text-white">
-                <img alt="AEC-logo" src="AEC_Logo.png" className="logo brightness-95 inline"/>
+                <img alt="AEC-logo" src="../assets/images/AEC_Logo.png" className="logo brightness-95 inline"/>
             </Link>
             <Link to="/home"
-                  className="rounded-lg px-3 py-2 text-white hover:text-gray-800 font-medium mr-4 transition duration-500 ease-in-out">
+                  className="rounded-lg px-3 py-2 text-black hover:text-red-800 font-medium mr-4 transition duration-500 ease-in-out">
                 Home
             </Link>
             <Link to="/about"
-                  className="rounded-lg px-3 py-2 text-white hover:text-gray-800 font-medium mr-4 transition duration-500 ease-in-out">
+                  className="rounded-lg px-3 py-2 text-black hover:text-red-800 font-medium mr-4 transition duration-500 ease-in-out">
                 About
             </Link>
         </div>
         <div className="flex float-right items-center">
             <Link to="/"
-                  className="rounded-lg px-3 py-2 text-white hover:text-gray-800 font-medium mr-4 transition duration-500 ease-in-out">
+                  className="rounded-lg px-3 py-2 text-black hover:text-red-800 font-medium mr-4 transition duration-500 ease-in-out">
                 Login
             </Link>
             <Link to="/register"
-                  className="rounded-lg px-3 py-2 text-white hover:text-gray-800 font-medium mr-4 transition duration-500 ease-in-out">
+                  className="rounded-lg px-3 py-2 text-black hover:text-red-800 font-medium mr-4 transition duration-500 ease-in-out">
                 Register
             </Link>
         </div>
