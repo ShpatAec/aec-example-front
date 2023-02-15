6107c1e277e4ab1af1e8839d0e9459107107d4b9
Nafie Sallahu <123952707+nafiesallahu1@users.noreply.github.com>
2023-02-15 10:25:21 +0100

    Revert "Improve cards content"
    This reverts commit 32d31846aa26a8807a4488ca34b80d68a7c7a9b7.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -40,17 +40,17 @@ function Home() {
             )}
             {fetchState === FetchState.SUCCESS && (
                 <>
-                    <div
-                        className="grid grid-flow-row gap-4  text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-10">
+                    <div className={HomeStyle.cards}>
+
                         {posts.map((post) => (
                             <div key={post.id}
-                                 className=" rounded-lg shadow-lg bg-white p-5 mb-2 bg-white border border-r border-b border-l border-gray-200  bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 ">
-                                <a href="/#">
-                                    <h5 className="mb-2 truncate text-2xl font-bold tracking-tight text-gray-900 ">  {post.title}</h5>
+                                 className="w-[280px] p-5 mb-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                <a href="#">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">  {post.title}</h5>
                                 </a>
-                                <p className={HomeStyle.truncateText}>{post.body}</p>
-                                <a href="/#"
-                                   className="w-30 inline-flex items-center px-3 py-2 font-medium text-xs text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.body}</p>
+                                <a href="#"
+                                   className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Read more
                                     <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor"
                                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
