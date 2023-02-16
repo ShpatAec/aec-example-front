5097c6e2ad4f957ffa8845c36db8506aa6505593
Nafie Sallahu <n.sallahu@aecsolutions.com>
2023-02-16 14:19:33 +0100

    Settings page added

--- src/pages/pagesData.tsx
+++ src/pages/pagesData.tsx
@@ -3,6 +3,9 @@ import About from "./About";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
+import AccountSettings from "./AccountSettings";
+import PasswordSettings from "./PasswordSettings";
+import Profile from "./Profile";
 import React from "react";
 
 // @ts-ignore
@@ -26,7 +29,23 @@ const pagesData: routerType[] = [
         path: "register",
         element: <Register/>,
         title: "register"
+    },
+    {
+        path: "accountSettings",
+        element: <AccountSettings/>,
+        title: "accountSettings"
+    },
+    {
+        path: "passwordSettings",
+        element: <PasswordSettings/>,
+        title: "passwordSettings"
+    },
+    {
+        path: "profile",
+        element: <Profile/>,
+        title: "profile"
     }
+
 ];
 
 export default pagesData;
