be74f3b91e152a4cfe627b31db6bb9e27adf485a
Nafie Sallahu <n.sallahu@aecsolutions.com>
2023-02-20 12:52:01 +0100

    Fixed Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,5 @@
-import {useGetPosts} from '../../lib/api-hooks';
-import {FetchState} from '../../types';
 import '../../App.css';
 import React from 'react';
-import HomeStyle from './home.module.css';
 import HomeTabs from "../HomeTabs";
 
 function Home() {
