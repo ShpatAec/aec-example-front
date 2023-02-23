dd0338bff4647c3276ed24ecc96205ea6aa04eeb
Arda Mazreku <a.mazreku@aecsolutions.com>
2023-02-23 13:53:53 +0100

    Fixes console log errors

--- src/pages/HomePageCards/index.tsx
+++ src/pages/HomePageCards/index.tsx
@@ -16,7 +16,7 @@ function HomePageCards() {
       .delete(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
         headers,
       })
-      .then((response) => {
+      .then(() => {
         console.log("deleted successfully!", postIdToDelete);
         const newPostsList = posts.filter((post) => post.id !== postIdToDelete);
         setPosts(newPostsList);
@@ -91,7 +91,7 @@ function HomePageCards() {
                       <path
                         fillRule="evenodd"
                         d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       ></path>
                     </svg>
                   </a>
