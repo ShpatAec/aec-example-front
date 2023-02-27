7690dca04710cbfb2419b5a1c40937bd3207e37b
Arda Mazreku <a.mazreku@aecsolutions.com>
2023-02-27 19:03:49 +0100

    Adds transition effect to hamburger

--- src/pages/Navbar/index.tsx
+++ src/pages/Navbar/index.tsx
@@ -190,7 +190,7 @@ const Navbar: React.FC = () => {
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
+              className="w-6 h-6 hover:text-red-500 transition duration-500 ease-in-out"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
