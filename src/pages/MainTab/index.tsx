340729ebfcb679f37daa70b7f189a4a04c0076da
Nafie Sallahu <n.sallahu@aecsolutions.com>
2023-03-03 11:26:50 +0100

    fixed error with href eslint

--- src/pages/MainTab/index.tsx
+++ src/pages/MainTab/index.tsx
@@ -68,7 +68,7 @@ function HomePageCards() {
                           ${post.price}
                         </td>
                         <td className="px-6 py-4">
-                          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                          <a href="/#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                         </td>
                       </tr>
                 ))}
