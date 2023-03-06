819b32aa8e535a52887f66ebbc1141a567eae7e8
Arda Mazreku <a.mazreku@aecsolutions.com>
2023-03-06 14:20:45 +0100

    Revert "Fixes tests"
    This reverts commit e12aabb4ca12e6079dc2649db07da1f14a61f2d5.

--- src/lib/api-hooks.ts
+++ src/lib/api-hooks.ts
@@ -1,37 +1,23 @@
 import axios from "axios";
 import { useState } from "react";
-import { FetchState, PostData, TableData } from "../types";
+import { FetchState, PostData } from "../types";
+
 export function useGetPosts() {
   const [fetchState, setFetchState] = useState(FetchState.DEFAULT);
   const [posts, setPosts] = useState<Array<PostData>>([]);
   const getPosts = async () => {
     try {
       setFetchState(FetchState.LOADING);
+
       const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       const resData = res.data as Array<PostData>;
+
       setPosts(resData);
       setFetchState(FetchState.SUCCESS);
     } catch (err) {
       setFetchState(FetchState.ERROR);
     }
   };
+
   return [posts, fetchState, getPosts, setPosts] as const;
-}
-export function useGeTableData() {
-  const [fetchState, setFetchState] = useState(FetchState.DEFAULT);
-  const [tableData, setTableData] = useState<Array<TableData>>([]);
-  const getTableData = async () => {
-    try {
-      setFetchState(FetchState.LOADING);
-      const res = await axios.get("https://fakestoreapi.com/products", {
-        timeout: 5000,
-      });
-      const resData = res.data as Array<TableData>;
-      setTableData(resData);
-      setFetchState(FetchState.SUCCESS);
-    } catch (err) {
-      setFetchState(FetchState.ERROR);
-    }
-  };
-  return [tableData, fetchState, getTableData, setTableData] as const;
 }
