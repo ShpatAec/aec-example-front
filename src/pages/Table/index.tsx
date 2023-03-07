a8d26d3a8dfcf7a4638939df4b2fc1be909d6a70
Arda Mazreku <a.mazreku@aecsolutions.com>
2023-03-07 15:38:31 +0100

    Removes svg element from table

--- src/pages/Table/index.tsx
+++ src/pages/Table/index.tsx
@@ -68,50 +68,12 @@ export default function TableComponent() {
         )}
         {fetchState === FetchState.SUCCESS && (
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <div className="flex items-center justify-between pb-2">
-        <button
-            id="dropdownRadioButton"
-            data-dropdown-toggle="dropdownRadio"
-            className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-            type="button"
-        >
-          <svg
-              className="w-4 h-4 mr-2 text-gray-400"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                clipRule="evenodd"
-            ></path>
-          </svg>
-          Last 30 days
-          <svg
-              className="w-3 h-3 ml-2"
-              aria-hidden="true"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M19 9l-7 7-7-7"
-            ></path>
-          </svg>
-        </button>
-        </div>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 700 }} className="dark:bg-gray-600" aria-label="customized table">
             <TableHead>
               <TableRow>
                 <StyledTableCell>Product name</StyledTableCell>
-                <StyledTableCell align="right">Description</StyledTableCell>
+                <StyledTableCell>Description</StyledTableCell>
                 <StyledTableCell align="right">Category</StyledTableCell>
                 <StyledTableCell align="right">Price</StyledTableCell>
               </TableRow>
@@ -125,7 +87,7 @@ export default function TableComponent() {
                               <StyledTableCell component="th" scope="row" className="dark:text-gray-300">
                                 {row.title}
                               </StyledTableCell>
-                              <StyledTableCell align="right" className="dark:text-gray-300">{row.description}</StyledTableCell>
+                              <StyledTableCell className="dark:text-gray-300">{row.description}</StyledTableCell>
                               <StyledTableCell align="right" className="dark:text-gray-300">{row.category}</StyledTableCell>
                               <StyledTableCell align="right" className="dark:text-gray-300">{row.price}</StyledTableCell>
                             </StyledTableRow>
